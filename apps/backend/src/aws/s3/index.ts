5118d39a99a6c648141900748dd28fa33765671a
Pratham mehta <mehtapratham49@gmail.com>
2025-03-02 15:36:59 +0530

    removing errors

--- apps/backend/src/aws/s3/index.ts
+++ apps/backend/src/aws/s3/index.ts
@@ -4,7 +4,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const s3client = new S3Client({
-  endpoint : process.env.END_POINT;
+  endpoint : process.env.END_POINT,
   region : "auto",
   credentials: {
     accessKeyId: process.env.ACCESS_KEY_ID!,
