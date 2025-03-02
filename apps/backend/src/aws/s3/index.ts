cf6a98a4dd747d1824b81b0c944e4fe7d328bece
Pratham mehta <mehtapratham49@gmail.com>
2025-03-02 15:43:28 +0530

    testing

--- apps/backend/src/aws/s3/index.ts
+++ apps/backend/src/aws/s3/index.ts
@@ -2,7 +2,7 @@ import { S3Client } from "@aws-sdk/client-s3";
 import dotenv from "dotenv";
 
 dotenv.config();
-
+ 
 export const s3client = new S3Client({
   endpoint : process.env.END_POINT,
   region : "auto",
